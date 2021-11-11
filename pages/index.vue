22808af6115002353bf0ebe9c6005b9f65294568
david istomin <daveistomin@gmail.com>
2021-11-11 15:33:10 +0400

    Evistep test.finished.

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Posts :posts="posts"></Posts>
+</template>
+
+<script>
+import Posts from "../components/Posts";
+export default {
+  components: {Posts},
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  methods: {
+    fetchPosts() {
+      this.$axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.posts = response.data
+      })
+    }
+  }
+}
+</script>
